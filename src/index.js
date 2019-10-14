8f3020b2552041c6d50d1f70cb81faf57b07c237
Andrew <andrew.alexander@ioc.sc>
2019-10-14 08:31:18 +0700

    add folder src

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const { pool } = require("./config");
+const { pool } = require("../config");
 
 const app = express();
 
